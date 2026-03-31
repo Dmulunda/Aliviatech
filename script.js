6b592761db015a8f66d57c4ae3a544de60990dae
Dmulunda <didier.mulunda@yahoo.com>
2026-03-31 13:47:26 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -143,3 +143,15 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-GJD7D0XFX9');
+
+
+// live chat widget
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/69cc03d47a2adf1c390a9d29/1jl2ettps';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
